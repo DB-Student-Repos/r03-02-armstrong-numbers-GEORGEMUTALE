c0aa2b4640176b2dca54c545aa8a5fbb9e3ef66e
GEORGEMUTALE <mutalegeorge367@gmail.com>
2024-05-12 10:26:56 +0200

    Armstrong number

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
-pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {num} is an armstrong number")
+pub fn is_armstrong_number(mut number: u32) -> bool {
+    let original_number = number;
+    let mut sum: u64 = 0;
+    let num_digits = (number as f64).log10() as u32 + 1;
+
+    while number != 0 {
+        let digit = number % 10;
+        sum += u64::pow(digit as u64, num_digits);
+        if sum > u64::from(original_number) {
+            return false; // Overflow occurred
+        }
+        number /= 10;
+    }
+
+    sum == u64::from(original_number)
 }
